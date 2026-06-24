9f073108acda00f4749d6a35e9e1ef0acce3b92b
SHIKHARGT03 <shikhargt0308@gmail.com>
2026-06-24 15:04:01 +0530

    done

--- ml-services/app.py
+++ ml-services/app.py
@@ -108,4 +108,5 @@ def brain_stroke_prediction():
         }), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
